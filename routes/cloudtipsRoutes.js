9130648f278afa71b8bd55c535ae0fa5e1278da3
MKNTW <maksim1771314@gmail.com>
2025-02-20 18:26:07 +0300

    Delete routes directory

--- routes/cloudtipsRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/cloudtipsRoutes.js
-const express = require('express');
-const router = express.Router();
-const cloudtipsController = require('../controllers/cloudtipsController');
-
-router.post('/complete', cloudtipsController.completeCloudtips);
-
-module.exports = router;
