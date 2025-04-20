3b0bd33d81fec0c73af1df92404eb8c9e0b5e04a
MKNTW <maksim1771314@gmail.com>
2025-04-20 15:44:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ function showConnectionError(msg) {
 // === Supabase ===
 const supabaseUrl = 'https://hjdcryfdhqxazdllhjsv.supabase.co'; 
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhqZGNyeWZkaHF4YXpkbGxoanN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2MDQyOTUsImV4cCI6MjA1NDE4MDI5NX0.yGYZ2_bkIismVidBNFYTdRRh1rZrfo1rT90UzNxhDWc';                
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 const STORAGE_BUCKET = "avatars";
 
